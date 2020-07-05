b3b80e34a06a0a78b4388d3932df2a1ae972950a
illiliti <illiliti@protonmail.com>
2020-07-05 12:30:53 +0300

    switch to macro

--- udev_list.c
+++ udev_list.c
@@ -20,14 +20,10 @@ void udev_list_entry_free(struct udev_list_entry *list_entry)
 
 void udev_list_entry_free_all(struct udev_list_entry *list_entry)
 {
-    struct udev_list_entry *tmp, *tmp2;
+    struct udev_list_entry *tmp;
 
-    tmp = list_entry;
-
-    while (tmp) {
-        tmp2 = tmp;
-        tmp = tmp->next;
-        udev_list_entry_free(tmp2);
+    udev_list_entry_foreach(tmp, list_entry) {
+        udev_list_entry_free(tmp);
     }
 }
 
@@ -77,14 +73,10 @@ UDEV_EXPORT struct udev_list_entry *udev_list_entry_get_by_name(struct udev_list
         return NULL;
     }
 
-    tmp = list_entry;
-
-    while (tmp) {
+    udev_list_entry_foreach(tmp, list_entry) {
         if (strcmp(tmp->name, name) == 0) {
             return tmp;
         }
-
-        tmp = tmp->next;
     }
 
     return NULL;
